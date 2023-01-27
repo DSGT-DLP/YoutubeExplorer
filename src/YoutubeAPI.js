c160cfa4517aced581c19cedef682aed57a857b0
karkir0003 <karkir0003@gmail.com>
2023-01-26 22:45:51 -0500

    backend skeleton

--- src/YoutubeAPI.js
+++ src/YoutubeAPI.js
@@ -2,3 +2,21 @@
  * This file will contain any code that you write to talk to the Youtube API
  */
 
+
+/**
+ * Get the channel id associated with the name of a Youtube Channel
+ * @param {*} channel_name - name of youtube channel based on what user enters
+ * Helpful starting point: https://developers.google.com/youtube/v3/docs/search
+ */
+function getChannelId(channel_name) {
+    //YOUR CODE HERE
+}
+
+/**
+ * Given the channel id, get all videos associated with the channel
+ * @param {*} channel_id - channel id
+ * What can we glean from one Youtube video in the API: https://developers.google.com/youtube/v3/docs/videos
+ */
+function getVideosInChannel(channel_id) {
+    //YOUR CODE HERE
+}
